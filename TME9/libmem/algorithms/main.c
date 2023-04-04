cd1bb6b289dd445c1db94750ce8aac167755585c
NYABANGANG <28603668@ppti-14-307-09.ufr-info-p6.jussieu.fr>
2023-04-04 17:38:17 +0200

    premier rendu tme8 encore vide

---
+++ TME9/libmem/algorithms/main.c
@@ -0,0 +1,21 @@
+#include "libmem.h"
+#include "Random.h"
+#include "LFU.h"
+
+int main(int argc,char*argv[]){
+	unsigned int frame_nb;
+	Swapper	s;	
+
+	scanf("%i",&frame_nb);
+
+	/* Initialisation du Swapper de la strat�gie LFU */
+	//initRandomSwapper(&s,frame_nb);
+	initLFUSwapper(&s,frame_nb);
+
+	if(swapSimulation(&s,stdin)<0){
+		printf("Error during swap simulation !!!\n");
+		return -1;
+	}
+
+	return 0;
+}
